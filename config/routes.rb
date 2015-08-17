dd7c5046494aab560ea690cfd13ad42e513e8697
Timothy Orbasido <t.orbasido@gmail.com>
2015-08-17 22:40:33 +0000

    add users and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 RottenMangoes::Application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'users/new'
+
+  get 'users/create'
+
   resources :movies
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
 end
