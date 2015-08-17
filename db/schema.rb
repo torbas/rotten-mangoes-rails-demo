650a44a863ebaa36af78527f42d47dceec3406b2
Timothy Orbasido <t.orbasido@gmail.com>
2015-08-17 21:42:17 +0000

    create user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150817191601) do
+ActiveRecord::Schema.define(version: 20150817214048) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20150817191601) do
     t.datetime "release_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "firstname"
+    t.string   "lastname"
   end
 
 end
