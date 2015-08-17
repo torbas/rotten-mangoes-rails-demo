44390756b23ff460fa2daf5957d635cacf972bff
Timothy Orbasido <t.orbasido@gmail.com>
2015-08-17 22:43:27 +0000

    add log in, logout, sign out

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ RottenMangoes::Application.routes.draw do
 
   resources :movies
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 end
