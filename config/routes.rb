d83f60f2ff07137018f7d9ae9a51f14b6e042e99
Timothy Orbasido <t.orbasido@gmail.com>
2015-08-18 22:45:44 +0000

    add admin to users

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ RottenMangoes::Application.routes.draw do
   resources :movies do
     resources :reviews, only: [:new, :create]
   end
+  namespace :admin do
+    resources :users
+  end
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
   root to: 'movies#index'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818214435) do
+ActiveRecord::Schema.define(version: 20150818224306) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20150818214435) do
     t.datetime "updated_at"
     t.string   "firstname"
     t.string   "lastname"
+    t.boolean  "admin",           default: false
   end
 
 end
