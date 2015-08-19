050f24db65bb9dc8851cbb9064f2b1bbf933c392
Timothy Orbasido <t.orbasido@gmail.com>
2015-08-19 22:48:40 +0000

    add destroy to admin/users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,4 +5,10 @@ class Admin::UsersController < ApplicationController
     @users = User.order("firstname").page(params[:page]).per(10)
   end
 
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to admin_users_path
+  end
+
 end
