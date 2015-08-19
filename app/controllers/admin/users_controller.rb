21405a7706704fbf91cd19f86f2a0a8ac882a6d8
Timothy Orbasido <t.orbasido@gmail.com>
2015-08-19 02:19:25 +0000

    pagination enable

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,7 @@ class Admin::UsersController < ApplicationController
   before_filter :restrict_access
 
   def index
-    @users = User.all
+    @users = User.order("firstname").page(params[:page]).per(10)
   end
 
 end
